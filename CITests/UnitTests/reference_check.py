c0661483351fd2c1d869c5e6138b0090469125e0
Sven-Heiko Hinrichs <sven.hinrichs@rwth-aachen.de>
2022-11-08 23:14:29 +0100

    Update

--- CITests/UnitTests/reference_check.py
+++ CITests/UnitTests/reference_check.py
@@ -737,10 +737,10 @@ if __name__ == '__main__':
             if args.batch is False:
                 print(f'All Reference files exist')
                 val = 0
-            if args.modified_models is False:
+            elif args.modified_models is False:
                 print(f'{conf.CRED}Error:{conf.CEND} Package is missing! (e.g. Airflow)')
                 val = 1
-            if args.modified_models is True:
+            elif args.modified_models is True:
                 print(f'No changed models in Package {args.single_package}')
                 val = 0
         else:
